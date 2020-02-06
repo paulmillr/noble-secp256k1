6306ea9566760b22c73967cd02a59966f850619b
Paul Miller <paul@paulmillr.com>
2020-02-06 12:13:18 +0200

    Update deps, precomputes file.

--- generate-precomputes.js
+++ generate-precomputes.js
@@ -2,23 +2,25 @@
 // Not included in NPM distribution to simplify code.
 // Precomputes are initially computed powers of two (2^120, 2^121 etc)
 // Which are used to speed-up calculations of elliptic curve cryptography.
-const fs = require("fs");
-const path = require("path");
-const { GG, multiple } = require("./index");
+const {writeFileSync} = require("fs");
+const sysPath = require("path");
+const { BASE_POINT } = require("./index");
 
-let precomputeContent = `import { Point } from "./point";
+const output = sysPath.join(__dirname, "./precomputed.ts");
+
+let content = `import { Point } from "./point";
 export default [`
 
-for (let i = 0; i < 257; i++) {
+for (let i = 0; i <= 256; i++) {
   const multiplier = 2n ** BigInt(i);
-  const result = multiple(GG, multiplier);
-  precomputeContent = `${precomputeContent}
+  const result = BASE_POINT.multiply(multiplier);
+  content = `${content}
   new Point(
     ${result.x}n,
     ${result.y}n,
   ),`
 }
 
-precomputeContent += "\n];";
+content += "\n];";
 
-fs.writeFileSync(path.join(__dirname, "./precomputed.ts"), precomputeContent);
+writeFileSync(output, content);
