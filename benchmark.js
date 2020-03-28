714cd5409a608b7697d6fa779ce5e052b52a30e4
Paul Miller <paul@paulmillr.com>
2020-03-28 20:33:16 +0200

    Add benchmarkjs.

--- benchmark.js
+++ benchmark.js
@@ -1,54 +1,75 @@
 let secp;
 
-function logMem(i) {
+function time() {
+  return process.hrtime.bigint();
+}
+
+function logMem() {
   const vals = Object.entries(process.memoryUsage()).map(([k, v]) => {
-    return `${k}=${(`${(v / 1e6).toFixed(1)}M`).padEnd(7)}`;
+    return `${k}=${`${(v / 1e6).toFixed(1)}M`.padEnd(7)}`;
   });
-  console.log(String(i).padStart(6), ...vals);
+  console.log('RAM:', ...vals);
 }
 
-function bench(name, counts, callback) {
-  const label = `${name} x${counts}`;
-  console.time(label);
-  for (let i = 0; i < counts; i++) {
-    callback();
+async function bench(label, samples, callback) {
+  const [μs, ms, sec] = [1000n, 1000000n, 1000000000n];
+  // if (counts > 1) label += ` x${counts}`;
+  const start = time();
+  for (let i = 0; i < samples; i++) {
+    let val = callback();
+    if (val instanceof Promise) await val;
   }
-  console.timeEnd(label);
+  const end = time();
+  const total = end - start;
+  const perItem = total / BigInt(samples);
+
+  let perItemStr = perItem.toString();
+  let symbol = 'ns';
+  if (perItem > μs) {
+    symbol = 'μs';
+    perItemStr = (perItem / μs).toString();
+  }
+  if (perItem > ms) {
+    symbol = 'ms';
+    perItemStr = (perItem / ms).toString();
+  }
+
+  const perSec = (sec / perItem).toString();
+  console.log(`${label} x ${perSec} ops/sec, ${perItemStr}${symbol} / op, ${samples} samples`);
 }
 
-// warm-up
-let pub;
-console.log('Starting');
-bench('load', 1, () => {
-  secp = require('.');
-  secp.utils.precompute(4);
-});
+(async () => {
+  // warm-up
+  let pub;
+  console.log('Benchmarking...\n');
+  await bench('load', 1, () => {
+    secp = require('.');
+    secp.utils.precompute(4);
+  });
 
-logMem('start');
-bench('getPublicKey 1 bit', 1, () => {
-  pub = secp.getPublicKey(2n);
-});
+  logMem('start');
+  console.log();
 
-// console.profile('cpu');
-const priv = 2n ** 255n + 12341n;
-bench('getPublicKey 256 bit', 1, () => {
-  pub = secp.getPublicKey(priv);
-});
+  await bench('getPublicKey 1 bit', 100, () => {
+    pub = secp.getPublicKey(2n);
+  });
 
-bench('sign', 1, async () => {
-  const s = Date.now();
-  const full = await secp.sign('beef', 4321n, { canonical: true });
-  console.log(Date.now() - s);
-});
+  // console.profile('cpu');
+  const priv = 2n ** 255n + 12341n;
+  await bench('getPublicKey 256 bit', 100, () => {
+    pub = secp.getPublicKey(priv);
+  });
 
-let custom = secp.Point.fromHex(pub);
-bench('multiply custom point', 1, () => {
-  pub = custom.multiply(0x0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdefn);
-});
+  await bench('sign', 100, async () => {
+    const s = Date.now();
+    const full = await secp.sign('beef', 4321n, { canonical: true });
+  });
 
-secp.Point.adds = 0;
-// secp.getPublicKey('0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef');
-console.log('adds', secp.Point.adds);
+  let custom = secp.Point.fromHex(pub);
+  await bench('multiply custom point', 100, () => {
+    pub = custom.multiply(0x0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdefn);
+  });
 
-logMem('end');
-// console.profileEnd('cpu');
+  console.log();
+  logMem();
+})();
