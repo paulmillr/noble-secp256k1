73cf04ef4f1d61b795a37cdb297b439d8b4ccbb6
Paul Miller <paul@paulmillr.com>
2023-01-06 11:09:02 +0000

    Bench: compressed keys

--- test/benchmark.js
+++ test/benchmark.js
@@ -34,13 +34,13 @@ run(async (windowSize) => {
   //   secp.getPublicKey('7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffcfcb');
   // });
   await mark('getPublicKey(utils.randomPrivateKey())', 5000, () => {
-    secp.getPublicKey(secp.utils.randomPrivateKey());
+    secp.getPublicKey(secp.utils.randomPrivateKey(), true);
   });
 
   const priv = 'f6fc7fd5acaf8603709160d203253d5cd17daa307483877ad811ec8411df56d2';
-  const pub = secp.getPublicKey(priv, false);
+  const pub = secp.getPublicKey(priv, true);
   const priv2 = '2e63f49054e1e44ccc2e6ef6ce387936efb16158f89cc302a2426e0b7fd66f66';
-  const pub2 = secp.getPublicKey(priv2, false);
+  const pub2 = secp.getPublicKey(priv2, true);
   const msg = 'deadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef';
   const signature = await secp.sign(msg, priv);
 
