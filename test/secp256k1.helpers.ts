64d4f6680fb2d02ccb0f1189ef601f5812cc8139
legobt <6wbvkn0j@anonaddy.me>
2023-05-03 03:02:51 +0000

    test: don't polyfill crypto with webCrypto for all unit tests

--- test/secp256k1.helpers.ts
+++ test/secp256k1.helpers.ts
@@ -1,6 +1,3 @@
-import { webcrypto } from 'node:crypto';
-// @ts-ignore
-if (!globalThis.crypto) globalThis.crypto = webcrypto; // @ts-ignore
 // @ts-ignore
 export * as secp from '../index.js';
 // @ts-ignore
