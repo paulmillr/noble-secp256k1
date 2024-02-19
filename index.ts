3db223351b80cc8c59b7196b5b92e0b1f3b0f93d
Paul Miller <paul@paulmillr.com>
2024-02-19 20:04:46 +0100

    Fix formatting

--- index.ts
+++ index.ts
@@ -27,8 +27,8 @@ class Point {                                           // Point in 3d xyz proje
   static readonly BASE = new Point(Gx, Gy, 1n);         // Generator / base point
   static readonly ZERO = new Point(0n, 1n, 0n);         // Identity / zero point
   static fromAffine (p: AffinePoint) {
-    if ((p.x === 0n) && (p.y === 0n)) return new Point (0n , 1n , 0n );
-    else return new Point ( p.x , p.y , 1n ) ;
+    if ((p.x === 0n) && (p.y === 0n)) return Point.ZERO;
+    else return new Point(p.x, p.y, 1n) ;
   }
   static fromHex(hex: Hex): Point {                     // Convert Uint8Array or hex string to Point
     hex = toU8(hex);                                    // convert hex string to Uint8Array
