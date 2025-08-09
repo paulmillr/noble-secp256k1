d5dd33dd2dddc701ebf94cd5e81a2546b8fce0b0
Paul Miller <paul@paulmillr.com>
2025-08-09 13:22:17 +0000

    Bump devdeps

--- test/build/input.js
+++ test/build/input.js
@@ -1,1 +1,2 @@
-export { getPublicKey, hashes, keygen, schnorr, sign, utils, verify } from "../../index.js";
+// export { getPublicKey, hashes, keygen, schnorr, sign, utils, verify } from "../../index.js";
+export { getPublicKey, hashes, keygen, schnorrAsync, signAsync, utils, verifyAsync } from "../../index.js";
